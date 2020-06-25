9633ff469f3250bc137058df615216ca7e60155c
aedifex <chris@circleci.com>
2020-06-25 12:43:02 -0700

    e2e demo

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "example" {
   vpc_security_group_ids = [aws_security_group.instance.id]
   user_data              = <<-EOF
               #!/bin/bash
-              echo "<h1>Hello from CircleCI</h1>" > index.html
+              echo "<h1>Hello from CircleCI!</h1>" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
   tags = {
